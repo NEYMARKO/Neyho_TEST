9cd383625bcbd4c90f1c0efefe27dc38e26f8ca2
NEYMARKO <marko.prosenjak5@gmail.com>
2025-09-01 13:38:35 +0200

    Sorting differences to added and missing, solved issue with program not detecting difference when reference file has less keys than comparison file

--- json_difference_comparison.py
+++ json_difference_comparison.py
@@ -17,33 +17,42 @@ def compare_json_files(reference_value : dict|str|float|list,
                        path : str, diffs : list[str]) -> None:
     #both values are dicts => go deeper in the structure
     if isinstance(reference_value, dict) and isinstance(comparison_value, dict):
-        for ref_key, ref_value in reference_value.items():
-            if ref_key not in comparison_value:
-                print("append to diffs 1")
-                diffs.append(path)
-                return
-            else:
-                path += f"{"." if path != "" else ""}{ref_key}"
-                compare_json_files(ref_value, comparison_value.get(ref_key), path, diffs)
-                path = path.replace(ref_key, "")
+        all_keys = set(reference_value.keys()) |set(comparison_value.keys())
+        for ref_key in all_keys:
+            ref_value = reference_value.get(ref_key)
+            comp_value = comparison_value.get(ref_key)
+
+            if not ref_value:
+                diffs.append({"ADDED" : f"{path}{'->' if path else ''}{ref_key}"})
+            if not comp_value:
+                diffs.append({"MISSING" : f"{path}{'->' if path else ''}{ref_key}"})
+
+            # if ref_key not in comparison_value:
+            #     # print(f"ref key: {ref_key}, comp_value: {comparison_value}")
+            #     # print(f"HERE: {path}")
+            #     diffs.append(path)
+            #     return
+            # else:
+            if ref_value and comp_value:
+                new_path = f"{path}{"->" if path != "" else ""}{ref_key}"
+                compare_json_files(ref_value, comparison_value.get(ref_key), new_path, diffs)
+                # path = path.replace(ref_key, "")
     
     #neither value is dict => they should be compared
     elif not isinstance(reference_value, dict) and not isinstance(comparison_value, dict):
         if reference_value != comparison_value:
             diffs.append(path)
-            print("append 2")
             return
         else:
             return
     #either reference_value or comparison value is dict, while the other is not
     else:
         diffs.append(path)
-        print("append 3")
         return 
 
 def main():
     reference_value = {'a': 2, 'b': { 'b1': 2.1 }, 'c' : { 'c1' : { 'c2' : 2.11 } } }
-    comparison_value = {'a': 2, 'b': { 'b1': 2.2 }, 'c' : { 'c2' : { 'c2' : 2.711 } } }
+    comparison_value = {'a': 2, 'b': { 'b1': 2.2, 'b2': 2.3 }, 'c' : { 'c2' : { 'c2' : 2.711 } } }
     diffs = []
     path = ""
     compare_json_files(reference_value, comparison_value, path, diffs)
