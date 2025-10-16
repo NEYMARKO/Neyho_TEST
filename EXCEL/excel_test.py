1ec88a82cfc0aa663d533950d5f3a96887680f4d
NEYMARKO <marko.prosenjak@neyho.com>
2025-10-16 15:03:23 +0200

    Saving and closing data

--- EXCEL/excel_test.py
+++ EXCEL/excel_test.py
@@ -337,6 +337,9 @@ def fill_cumulative_table(excel_app : any, employees_data : list[Employee]) -> N
         insertion_data.append((row, employee_cumulatives))
         # print(f"MODIFIED MONTH REPORT: {e.monthly_data}")
     batch_rows_insert(start_col=CUMULATIVE_STARTING_COL, end_col=columns_count, data=insertion_data, ws=ws)
+    wb.Save()
+    excel_app.closeFile(CUMULATIVES_PATH)
+    print(f"INSERTED cumulatives")
     return
 
 def main():
