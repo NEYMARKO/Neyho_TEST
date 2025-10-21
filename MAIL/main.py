81e3ff3d72b06a82327d10e48642edfe3d35b959
NEYMARKO <marko.prosenjak@neyho.com>
2025-10-21 12:07:43 +0200

    Works for case when there isn't net - last fetched delta/next link gets written to cfg file

--- MAIL/main.py
+++ MAIL/main.py
@@ -32,6 +32,7 @@ RECIPIENTS_LIST = ["b247c66a-2651-4ff9-a6a1-858a24f30387"]
 #         message: None
 #         error: MainError(additional_data={}, code='ErrorItemNotFound', details=None, inner_error=None, message='The specified object was not found in the store., The process failed to get the correct properties.', target=None)
 
+CONFIG_FILE_NAME = 'config.cfg'
 
 async def display_access_token(graph: Graph) -> None:
     token = await graph.get_app_only_token()
@@ -42,6 +43,14 @@ def write_to_cfg(file_name : str, config : configparser, section_name : str) -> 
     with open(file_name, 'w') as config_file:
        config.write(config_file)
     print(f"Updated delta link to value: {config[section_name].get('delta_value')}")
+
+def add_section_to_config(config : configparser, section_name : str) -> None:
+    if not config.has_section(section_name):
+        config.add_section(section_name)
+
+def add_value_to_section(config : configparser, section_name : str, value_key : str, value : str | int) -> None:
+    add_section_to_config(config, section_name)
+    config[section_name][value_key] = "" if value is None else value
 
 async def main():
     print('Python Graph Tutorial\n')
@@ -55,31 +64,45 @@ async def main():
     
     token = None
     delta_link = None
-    while True:
-        if not token:
-            token = await graph.get_app_only_token()
-        for recipient_id in RECIPIENTS_LIST:
-            section_name = f'delta.{recipient_id}'
-            if config.has_section(section_name):
-                delta_link = config[section_name].get('delta_value', '')
-            all_messages = []
-            response = await graph.get_mails(recipient_id=recipient_id, delta_url=delta_link)
-            all_messages.extend(response.value)
-            next_link = response.odata_next_link
-            while(next_link):
-                response = await graph.get_mails(recipient_id=recipient_id, delta_url=next_link)
+    recipient_id = None
+    section_name = None
+    next_link = None
+    try:
+        while True:
+            if not token:
+                token = await graph.get_app_only_token()
+            for recipient_id in RECIPIENTS_LIST:
+                section_name = f'delta.{recipient_id}'
+                next_link = None
+                #delta link has to be updated on every startup - can't be shared between multiple e-mail
+                #addresses. Even if there was 1 address, it would still need to be updated every run
+                if config.has_section(section_name):
+                    delta_link = config[section_name].get('delta_value', '')
+                all_messages = []
+                response = await graph.get_mails(recipient_id=recipient_id, delta_url=delta_link)
                 all_messages.extend(response.value)
                 next_link = response.odata_next_link
-            await graph.download_attachments(messages=all_messages, recipient_id=recipient_id)
-            if not config.has_section(section_name):
-                config.add_section(section_name)
-            config[section_name]['delta_value'] = response.odata_delta_link
-            if len(all_messages) > 0:
-                write_to_cfg('config.cfg', config, section_name)
-        print("\n" * 2)
-        print("-*-" * 50)
-        sleep(5)
-        
+                while(next_link):
+                    print(f"\n\nNEXT LINK: {next_link}\n\n")
+                    add_value_to_section(config, section_name, 'delta_value', next_link)
+                    response = await graph.get_mails(recipient_id=recipient_id, delta_url=next_link)
+                    all_messages.extend(response.value)
+                    next_link = response.odata_next_link
+                await graph.download_attachments(messages=all_messages, recipient_id=recipient_id)
+                delta_link = response.odata_delta_link
+                add_value_to_section(config, section_name, 'delta_value', delta_link)
+                if len(all_messages) > 0:
+                    write_to_cfg(CONFIG_FILE_NAME, config, section_name)
+            print("\n" * 2)
+            print("-*-" * 50)
+            sleep(5)
+    
+    except Exception:
+        print("------------------EXCEPTION OCCURED, perserving last delta link------------------")
+        print(f"{section_name=}\n{next_link=}\n{delta_link=}")
+        add_value_to_section(config, section_name, 'delta_value', next_link if next_link else delta_link)
+        write_to_cfg(CONFIG_FILE_NAME, config, section_name)
+
     await graph.close()
     return
 
