53c0dea2deb46bd4fad31d2e69bcc96795a1246a
NEYMARKO <marko.prosenjak@neyho.com>
2026-01-10 15:29:04 +0100

    Extracting just table from original image and managing perspective of it

--- A1-MAC/table_content_extractor.py
+++ A1-MAC/table_content_extractor.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 from enum import Enum
 from pathlib import Path
 
@@ -23,8 +24,8 @@ class TableExtractor:
     
     def find_contours(self) -> None:
         self.contours, self.hierarchy = cv2.findContours(self.preprocessed_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        self.image_with_all_contours = self.original_image.copy()
-        cv2.drawContours(self.image_with_all_contours, self.contours, -1, (0, 255, 0), 3)
+        self.original_image_with_all_contours = self.original_image.copy()
+        cv2.drawContours(self.original_image_with_all_contours, self.contours, -1, (0, 255, 0), 3)
     
     def filter_contours_and_leave_only_rectangles(self):
         self.rectangular_contours = []
@@ -35,8 +36,8 @@ class TableExtractor:
                 self.rectangular_contours.append(approx)
         # Below lines are added to show all rectangular contours
         # This is not needed, but it is useful for debugging
-        self.image_with_only_rectangular_contours = self.original_image.copy()
-        cv2.drawContours(self.image_with_only_rectangular_contours, self.rectangular_contours, -1, (0, 255, 0), 3)
+        self.original_image_with_only_rectangular_contours = self.original_image.copy()
+        cv2.drawContours(self.original_image_with_only_rectangular_contours, self.rectangular_contours, -1, (0, 255, 0), 3)
 
     def find_largest_contour_by_area(self):
         max_area = 0
@@ -48,8 +49,8 @@ class TableExtractor:
                 self.contour_with_max_area = contour
         # Below lines are added to show the contour with max area
         # This is not needed, but it is useful for debugging
-        self.image_with_contour_with_max_area = self.original_image.copy()
-        cv2.drawContours(self.image_with_contour_with_max_area, [self.contour_with_max_area], -1, (0, 255, 0), 3)
+        self.original_image_with_contour_with_max_area = self.original_image.copy()
+        cv2.drawContours(self.original_image_with_contour_with_max_area, [self.contour_with_max_area], -1, (0, 255, 0), 3)
 
     def find_and_filter_contours(self) -> None:
         """
@@ -58,6 +59,59 @@ class TableExtractor:
         3. Finds the largest contour by area - is presumed to be the table.
         """
         return
+
+    def add_10_percent_padding(self):
+        image_height = self.original_image.shape[0]
+        padding = int(image_height * 0.1)
+        self.perspective_corrected_image_with_padding = cv2.copyMakeBorder(self.perspective_corrected_image, padding, padding, padding, padding, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+
+    def order_points_in_the_contour_with_max_area(self):
+        self.contour_with_max_area_ordered = self.order_points(self.contour_with_max_area)
+        # The code below is to plot the points on the image
+        # it is not required for the perspective transform
+        # it will help you to understand and debug the code
+        self.original_image_with_points_plotted = self.original_image.copy()
+        for point in self.contour_with_max_area_ordered:
+            point_coordinates = (int(point[0]), int(point[1]))
+            self.original_image_with_points_plotted = cv2.circle(self.original_image_with_points_plotted, point_coordinates, 10, (0, 0, 255), -1)
+    def calculate_new_width_and_height_of_image(self):
+        existing_image_width = self.original_image.shape[1]
+        existing_image_width_reduced_by_10_percent = int(existing_image_width * 0.9)
+        
+        distance_between_top_left_and_top_right = self.calculateDistanceBetween2Points(self.contour_with_max_area_ordered[0], self.contour_with_max_area_ordered[1])
+        distance_between_top_left_and_bottom_left = self.calculateDistanceBetween2Points(self.contour_with_max_area_ordered[0], self.contour_with_max_area_ordered[3])
+        aspect_ratio = distance_between_top_left_and_bottom_left / distance_between_top_left_and_top_right
+        self.new_image_width = existing_image_width_reduced_by_10_percent
+        self.new_image_height = int(self.new_image_width * aspect_ratio)
+    def apply_perspective_transform(self):
+        pts1 = np.float32(self.contour_with_max_area_ordered)
+        pts2 = np.float32([[0, 0], [self.new_image_width, 0], [self.new_image_width, self.new_image_height], [0, self.new_image_height]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        self.perspective_corrected_image = cv2.warpPerspective(self.original_image, matrix, (self.new_image_width, self.new_image_height))
+    # Below are helper functions
+    def calculateDistanceBetween2Points(self, p1, p2):
+        dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+        return dis
+    def order_points(self, pts):
+        # initialzie a list of coordinates that will be ordered
+        # such that the first entry in the list is the top-left,
+        # the second entry is the top-right, the third is the
+        # bottom-right, and the fourth is the bottom-left
+        pts = pts.reshape(4, 2)
+        rect = np.zeros((4, 2), dtype="float32")
+        # the top-left point will have the smallest sum, whereas
+        # the bottom-right point will have the largest sum
+        s = pts.sum(axis=1)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        # now, compute the difference between the points, the
+        # top-right point will have the smallest difference,
+        # whereas the bottom-left will have the largest difference
+        diff = np.diff(pts, axis=1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        # return the ordered coordinates
+        return rect
 
     def show_image(self, type : ImageType) -> None:
         match type:
--- A1-MAC/table_tester.py
+++ A1-MAC/table_tester.py
@@ -8,9 +8,14 @@ def main():
     tableExtractor.show_image(ImageType.PREPROCESSED)
     tableExtractor.find_contours()
     tableExtractor.filter_contours_and_leave_only_rectangles()
-    # tableExtractor.find_largest_contour_by_area()
-    cv2.imwrite(Path() / "outputs/contours/original_with_contours.png", tableExtractor.image_with_contour_with_max_area)
-    cv2.waitKey(0)
+    tableExtractor.find_largest_contour_by_area()
+    # cv2.imwrite(Path() / "outputs/contours/contours.png", tableExtractor.image_with_contour_with_max_area)
+    # cv2.waitKey(0)
+    tableExtractor.order_points_in_the_contour_with_max_area()
+    tableExtractor.calculate_new_width_and_height_of_image()
+    tableExtractor.apply_perspective_transform()
+    tableExtractor.add_10_percent_padding()
+    cv2.imwrite(Path() / "outputs/contours/perspective.png", tableExtractor.perspective_corrected_image_with_padding)
     return
 
 if __name__ == "__main__":
